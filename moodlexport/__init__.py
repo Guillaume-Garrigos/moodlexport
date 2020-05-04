42aff997b3dbd7fa1bac6e76cf5073f61465d23a
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2020-05-04 18:18:53 +0200

    Yet antoher forgotten mistake yay

--- moodlexport/__init__.py
+++ moodlexport/__init__.py
@@ -1,4 +1,4 @@
-from test_moodlexport.python_to_moodle import *
-from test_moodlexport.tex_to_python import *
+from moodlexport.python_to_moodle import *
+from moodlexport.tex_to_python import *
 
 #__all__ = [ Category]
