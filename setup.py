d1dd44589daadc1e434620f520ef1018b79b0336
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2020-05-24 20:55:58 +0200

    Specify that we only include .sty in template

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="moodlexport",
-    version="0.0.16",
+    version="0.0.17",
     author="Guillaume Garrigos",
     author_email="guillaume.garrigos@lpsm.paris",
     license="MIT",
