7ae8189600842787966be9a7c67701a0e4b15d3e
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2020-05-24 15:09:31 +0200

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="moodlexport",
-    version="0.0.13",
+    version="0.0.14",
     author="Guillaume Garrigos",
     author_email="guillaume.garrigos@lpsm.paris",
     license="MIT",
